8f9c45288f1bc628842747445e06b6bf9fdd01c5
1909Mohit <mohit1909.mk@gmail.com>
2022-07-13 23:41:09 +0530

    envfix

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const mongoose = require('mongoose');
 
-const dbUrl = 'mongodb://localhost:27017/inotebook';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/inotebook';
 
 const connectToMongo = () => {
     mongoose.connect(dbUrl, () => {
