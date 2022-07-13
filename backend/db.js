7fa4e0b5dd0011cb91a920a4fc6499d7931f7481
1909Mohit <mohit1909.mk@gmail.com>
2022-07-13 22:53:46 +0530

    resources

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const mongoose = require('mongoose');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/inotebook';
+const dbUrl = process.env.DB_URL 
 
 const connectToMongo = () => {
-    mongoose.connect(dbUrl, () => {
-        console.log("Database Connected !!");
-    })
+    mongoose.connect(dbUrl);
+    
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", () => {
+        console.log("Database connected");
+    });
 }
 
 module.exports = connectToMongo;
