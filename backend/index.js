8f91886af278e3d7c82dd2ba0c07fb956088509f
1909Mohit <mohit1909.mk@gmail.com>
2022-07-25 14:06:39 +0530

    index2

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const NotesRoutes = require('./routes/note');
 const app = express();
 
 
-const dbUrl = 'mongodb://localhost:27017/inotebook';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/inotebook';
 
 mongoose.connect(dbUrl, () => {
     console.log("Database Connected !!");
